a877361cd8907d5657676539beb42777adca5698
shri-hari27 <62689707+shri-hari27@users.noreply.github.com>
2020-09-23 19:41:02 +0530

    Add files via upload

---
+++ python1.py (1).py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+thriller=["prison break"," avengers end game"," infinity war","Inception"," men in black"," mission impossible","Money Heist","wanda vision"]
+comedy=["bean","johhny english","johhny english reborn","johnny english strikes again","rambo 2"]
+i=0
+movie=input("Enter the movie:")
+if movie==thriller[i]:
+    print("It is a thriller")
+elif movie==comedy[i]:
+    print("It is a comedy")
+else:
+    print("It is not a thriller nor a comedy")
+
+
+# In[ ]:
+
+
+
+
